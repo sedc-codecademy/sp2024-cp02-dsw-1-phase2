ac95d3230bffd273d49a8316e7d495aff5313bc4
Igor Veic <149272013+IgorVeic@users.noreply.github.com>
2024-09-30 23:31:35 +0200

    add setup common files

---
+++ qintronics-client/qintronics-app/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Ensure this matches the location of your HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // This covers all JS, TS, JSX, and TSX files in your src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
