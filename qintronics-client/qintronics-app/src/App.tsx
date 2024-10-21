0e37bf75fa6580d4ce2d7fbd437675340d102abf
davcevs <148985855+davcevs@users.noreply.github.com>
2024-10-21 20:37:05 +0200

    home page fixes

--- qintronics-client/qintronics-app/src/App.tsx
+++ qintronics-client/qintronics-app/src/App.tsx
@@ -23,6 +23,7 @@ import AboutUs from "./components/AboutUs";
 import FAQ from "./components/FAQ";
 import Shipping from "./components/Shipping";
 import Favorites from "./components/Favorites";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
@@ -33,58 +34,60 @@ function App() {
 
   return (
     <AuthContextProvider>
-      <div className="App flex flex-col">
-        <Header onLoginClick={toggleLoginPopup} />
+      <UserProvider>
+        <div className="App flex flex-col">
+          <Header onLoginClick={toggleLoginPopup} />
 
-        <div className="content grow">
-          <CardPaymentProvider>
-            <Routes>
-              <Route path="/" element={<MainComponent />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route element={<PrivateRoute />}>
-                <Route path="/checkout" element={<OrderPage />} />
-                <Route path="/payment" element={<CardPaymentForm />} />
-                <Route path="/favorites" element={<Favorites />} />
-              </Route>
-              <Route path="/contact" element={<ContactForm />} />
-              <Route path="/compare" element={<CompareProducts />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/products/:id" element={<ProductDetailsPage />} />
-              <Route path="/sales" element={<SalesPage />} />
-              <Route path="/category/:category" element={<CategoryPage />} />
-              <Route path="/category/gift-cards" element={<GiftCard />} />
-              <Route path="/about-us" element={<AboutUs />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/shipping" element={<Shipping />} />
-            </Routes>
-          </CardPaymentProvider>
+          <div className="content grow">
+            <CardPaymentProvider>
+              <Routes>
+                <Route path="/" element={<MainComponent />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route element={<PrivateRoute />}>
+                  <Route path="/checkout" element={<OrderPage />} />
+                  <Route path="/payment" element={<CardPaymentForm />} />
+                  <Route path="/favorites" element={<Favorites />} />
+                </Route>
+                <Route path="/contact" element={<ContactForm />} />
+                <Route path="/compare" element={<CompareProducts />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/products/:id" element={<ProductDetailsPage />} />
+                <Route path="/sales" element={<SalesPage />} />
+                <Route path="/category/:category" element={<CategoryPage />} />
+                <Route path="/category/gift-cards" element={<GiftCard />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/shipping" element={<Shipping />} />
+              </Routes>
+            </CardPaymentProvider>
+          </div>
+
+          <LoginPopup isOpen={isLoginPopupOpen} onClose={toggleLoginPopup} />
+          <Footer />
+
+          <div className="fixed bottom-4 right-4">
+            <motion.button
+              onClick={toggleChat}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="bg-blue-600 text-white p-4 rounded-full shadow-lg focus:outline-none"
+            >
+              💬 Chat
+            </motion.button>
+          </div>
+
+          {isChatOpen && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.3 }}
+              className="fixed bottom-16 right-4 w-80 h-96 bg-white border border-gray-300 shadow-lg rounded-lg"
+            >
+              <Chatbot toggleChat={toggleChat} />
+            </motion.div>
+          )}
         </div>
-
-        <LoginPopup isOpen={isLoginPopupOpen} onClose={toggleLoginPopup} />
-        <Footer />
-
-        <div className="fixed bottom-4 right-4">
-          <motion.button
-            onClick={toggleChat}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className="bg-blue-600 text-white p-4 rounded-full shadow-lg focus:outline-none"
-          >
-            💬 Chat
-          </motion.button>
-        </div>
-
-        {isChatOpen && (
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.3 }}
-            className="fixed bottom-16 right-4 w-80 h-96 bg-white border border-gray-300 shadow-lg rounded-lg"
-          >
-            <Chatbot toggleChat={toggleChat} />
-          </motion.div>
-        )}
-      </div>
+      </UserProvider>
     </AuthContextProvider>
   );
 }
